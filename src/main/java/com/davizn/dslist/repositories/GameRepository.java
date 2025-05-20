1eaba9602ca07010bd55bbb66bd01ae5162837b0
Davi Serra <daviserrafc@gmail.com>
2025-05-19 22:18:32 -0300

    Game repository, service, controller

---
+++ src/main/java/com/davizn/dslist/repositories/GameRepository.java
@@ -0,0 +1,8 @@
+package com.davizn.dslist.repositories;
+
+import com.davizn.dslist.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+}
