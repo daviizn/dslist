6d0d1fe5cc7f1404cce9d21459dfc0a2b2051855
Davi Serra <daviserrafc@gmail.com>
2025-05-22 17:52:36 -0300

    SQL, projection, GET games by list

--- src/main/java/com/davizn/dslist/services/GameService.java
+++ src/main/java/com/davizn/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.davizn.dslist.services;
 import com.davizn.dslist.dto.GameDTO;
 import com.davizn.dslist.dto.GameMinDTO;
 import com.davizn.dslist.entities.Game;
+import com.davizn.dslist.projections.GameMinProjection;
 import com.davizn.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
