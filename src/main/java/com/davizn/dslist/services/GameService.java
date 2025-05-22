c2f5f065c3c0d0b657435c1a0edab829a48d7004
Davi Serra <daviserrafc@gmail.com>
2025-05-22 08:28:46 -0300

    GET lists

--- src/main/java/com/davizn/dslist/services/GameService.java
+++ src/main/java/com/davizn/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.davizn.dslist.services;
 
+import com.davizn.dslist.dto.GameDTO;
 import com.davizn.dslist.dto.GameMinDTO;
 import com.davizn.dslist.entities.Game;
 import com.davizn.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
